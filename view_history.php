635dc4bbfe0e3191221bb952b9d5a750ea4e522e
jameslooi19 <32288974+jameslooi19@users.noreply.github.com>
2017-11-12 13:42:19 +0800

    update 2

--- view_history.php
+++ view_history.php
@@ -27,7 +27,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>HELPFit Member | View History</title>
+  <title>HELPFit Member | History</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
--- view_reviews.php
+++ view_reviews.php
@@ -18,7 +18,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>HELPFit | View Reviews</title>
+  <title>HELPFit | Reviews</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
